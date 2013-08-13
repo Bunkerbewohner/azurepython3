70179936d438328a018b1fd152e8ae5d4355c599
Mathias Kahl <mathias.kahl@googlemail.com>
2013-08-13 15:48:52 +0200

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 from distutils.core import setup
 
 setup(
-    name='AzurePython3',
+    name='azurepython3',
     version='0.1.0',
     author='Mathias Kahl',
     author_email='mathias.kahl@gmail.com',
     packages=['azurepython3'],
     scripts=[],
-    url='http://pypi.python.org/pypi/AzurePython3/',
-    license='LICENSE',
+    url='http://pypi.python.org/pypi/azurepython3/',
+    download_url='https://github.com/Bunkerbewohner/azurepython3',
+    license='MIT License',
     description='Incomplete Windows Azure library for Python 3',
     long_description=open('README.md').read(),
     install_requires=[
         "requests >= 1.2.3"
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3'
+    ]
 )
