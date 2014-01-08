18d14b188acdd5613ae6bf66596ffdc1a219bc6e
Mathias Kahl <mathias.kahl@gmail.com>
2014-01-08 17:01:18 +0100

    version incremented

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='azurepython3',
-    version='1.7.2',
+    version='1.7.3',
     author='Mathias Kahl',
     author_email='mathias.kahl@gmail.com',
     packages=['azurepython3'],
